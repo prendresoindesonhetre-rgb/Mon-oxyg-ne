7ade59e2e53c99ce4d336d3f2cdff402ed466d8b
prendresoindesonhetre-rgb <prendresoindesonhetre@gmail.com>
2026-08-20 00:01:58 +0200

    Finaliser l’intégration des deux visuels validés

--- app/src/main/java/fr/prendresoindesonhetre/monoxygene/BackgroundAssets.java
+++ app/src/main/java/fr/prendresoindesonhetre/monoxygene/BackgroundAssets.java
@@ -1,7 +1,7 @@
 package fr.prendresoindesonhetre.monoxygene;
 
 final class BackgroundAssets {
-    static final String[] SETTINGS = new String[]{ AssetSettings1.DATA, AssetSettings2.DATA, AssetSettings3.DATA, AssetSettings4.DATA };
+    static final String[] SETTINGS = new String[]{ AssetSettings1.DATA, AssetSettings2.DATA, AssetSettings3.DATA, AssetSettings4.DATA, AssetSettings5.DATA };
     static final String[] CURVE = new String[]{ AssetCurve1.DATA, AssetCurve2.DATA, AssetCurve3.DATA };
     private BackgroundAssets() {}
 }
