5ad8cecbf167a0bf88be9257ab68465693282f43
prendresoindesonhetre-rgb <prendresoindesonhetre@gmail.com>
2026-09-20 23:01:00 +0200

    Empêcher les barres système de recouvrir Mon Oxygène

--- webv40/app/src/main/java/fr/prendresoindesonhetre/monoxygene/v40/MainActivity.java
+++ webv40/app/src/main/java/fr/prendresoindesonhetre/monoxygene/v40/MainActivity.java
@@ -2,6 +2,7 @@ package fr.prendresoindesonhetre.monoxygene.v40;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Build;
 import android.graphics.Color;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,6 +18,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(lp);
+        }
         enterImmersiveMode();
 
         webView = new WebView(this);
@@ -49,9 +58,18 @@ public class MainActivity extends Activity {
 
     private void enterImmersiveMode() {
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) enterImmersiveMode();
     }
 
     @Override
