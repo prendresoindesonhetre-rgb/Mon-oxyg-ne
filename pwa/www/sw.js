3a96f1fee2201b5f89d78b055254c3493c220af8
prendresoindesonhetre-rgb <prendresoindesonhetre@gmail.com>
2026-09-20 22:01:49 +0200

    Renouveler le cache Mon Oxygène v63

--- pwa/www/sw.js
+++ pwa/www/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mon-oxygene-pwa-v62-sharp-ui';
+const CACHE='mon-oxygene-pwa-v63-custom-icons';
 const CORE=[
   './','./index.html','./app.js','./sequence.js','./orientation.js','./compat-init.js','./responsive.js','./v41-phone-ui.js','./v44-rainstick.js',
   './styles.css','./v59-settings-redesign.css','./v59-settings-redesign.js','./landscape-force.css','./legacy.css','./responsive.css','./v37-fixes.css','./v38-responsive.css','./v39-landscape-fit.css','./v40-mobile-sequence.css','./v41-phone-ui.css',
