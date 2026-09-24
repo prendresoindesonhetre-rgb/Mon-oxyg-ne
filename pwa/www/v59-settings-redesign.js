70cdb9e13616d570e714f5cac41c93a264f52a67
prendresoindesonhetre-rgb <prendresoindesonhetre@gmail.com>
2026-09-24 22:56:54 +0200

    Utiliser les nouvelles icônes des rythmes

--- pwa/www/v59-settings-redesign.js
+++ pwa/www/v59-settings-redesign.js
@@ -14,48 +14,16 @@
 
   function presetIcon(kind) {
     var icons = {
-      slow:
-        '<svg viewBox="0 0 64 64" aria-hidden="true">' +
-          '<path class="h-fill" d="M39 10c-10 1-19 6-24 14-4 6-4 13-2 18 7 1 14-1 20-6 7-6 10-16 9-25-1 0-2-1-3-1Z"/>' +
-          '<path class="h-vein" d="M39 14c-7 7-13 15-17 23M32 22l-9-1m13 8-9 2"/>' +
-          '<path class="h-violet" d="M46 21c3 8 0 17-7 24-6 6-14 9-22 9"/>' +
-          '<path class="h-violet h-dash" d="M49 24c2 5 2 9 0 13"/>' +
-          '<path class="h-violet-fill" d="M14 52l7-2-4 6Z"/>' +
-        '</svg>',
-      calm:
-        '<svg viewBox="0 0 64 64" aria-hidden="true">' +
-          '<ellipse class="h-ring" cx="32" cy="49" rx="22" ry="7"/>' +
-          '<ellipse class="h-ring2" cx="32" cy="49" rx="15" ry="4.5"/>' +
-          '<path class="h-fill" d="M38 13c-9 1-16 6-20 13-3 5-3 11-1 15 6 1 12-1 17-5 6-5 8-13 8-22-1 0-3-1-4-1Z"/>' +
-          '<path class="h-vein" d="M38 17c-6 6-11 12-14 20M32 24l-7-1m10 7-7 2"/>' +
-        '</svg>',
-      balance:
-        '<svg viewBox="0 0 64 64" aria-hidden="true">' +
-          '<path class="h-arc" d="M13 25c5-10 14-16 19-16s14 6 19 16"/>' +
-          '<path class="h-fill" d="M16 25c8 1 14 5 17 11-5 5-13 6-19 3-4-2-7-6-8-11 3-2 6-3 10-3Z"/>' +
-          '<path class="h-fill" d="M48 25c-8 1-14 5-17 11 5 5 13 6 19 3 4-2 7-6 8-11-3-2-6-3-10-3Z"/>' +
-          '<path class="h-vein" d="M14 29c6 3 11 7 16 13m20-13c-6 3-11 7-16 13"/>' +
-          '<circle class="h-violet-fill" cx="32" cy="36" r="3.5"/>' +
-          '<path class="h-violet" d="M32 40v10"/>' +
-        '</svg>',
-      energy:
-        '<svg viewBox="0 0 64 64" aria-hidden="true">' +
-          '<path class="h-stem" d="M24 49c7-11 12-22 18-34"/>' +
-          '<path class="h-fill small" d="M42 13c8 0 14 4 17 10-5 5-12 7-18 4-4-2-7-6-7-11 2-2 5-3 8-3Z"/>' +
-          '<path class="h-fill small" d="M25 29c-7 0-13 4-15 10 5 5 11 6 17 3 4-2 6-6 6-10-2-2-5-3-8-3Z"/>' +
-          '<path class="h-violet" d="M15 53c12-2 22-8 29-19"/>' +
-          '<path class="h-violet-fill" d="M45 29l2-8 5 6Z"/>' +
-          '<path class="h-spark" d="M49 10v-5m0 5 4-3m-4 3-4-3"/>' +
-        '</svg>',
-      dynamic:
-        '<svg viewBox="0 0 64 64" aria-hidden="true">' +
-          '<path class="h-fill" d="M38 15c-9 1-16 6-20 13-3 5-3 11-1 15 6 1 12-1 17-5 6-5 8-13 8-22-1 0-3-1-4-1Z"/>' +
-          '<path class="h-vein" d="M38 19c-6 6-11 12-14 20M32 26l-7-1m10 7-7 2"/>' +
-          '<path class="h-rays" d="M32 4v7M32 49v7M9 30h7m32 0h7M15 13l5 5m24 24 5 5M49 13l-5 5M20 42l-5 5"/>' +
-          '<path class="h-rays-short" d="M20 7l3 6m18-6-3 6M8 19l6 3m36-3-6 3"/>' +
-        '</svg>'
+      slow: './assets/presets/ralentir.png',
+      calm: './assets/presets/retrouver-calme.png',
+      balance: './assets/presets/equilibre.png',
+      energy: './assets/presets/retrouver-dynamisme.png',
+      dynamic: './assets/presets/dynamiser.png'
     };
-    return '<span class="v60-preset-icon v64-icon v64-icon-' + kind + '">' + icons[kind] + '</span>';
+    var src = icons[kind] || icons.balance;
+    return '<span class="v60-preset-icon v64-icon v64-icon-' + kind + '">' +
+      '<img class="v83-preset-image" src="' + src + '" alt="" aria-hidden="true" draggable="false">' +
+    '</span>';
   }
 
   function preparePreset(button, title, rhythm, iconKind) {
